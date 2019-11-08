fe0716df079f376b6d9a8485522261efb4322de7
ye <767439886@qq.com>
2019-11-08 18:07:44 +0800

    注册后台

--- src/main/java/com/example/driverschool/controller/register.java
+++ src/main/java/com/example/driverschool/controller/register.java
@@ -3,7 +3,9 @@ package com.example.driverschool.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.example.driverschool.common.Result ;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @RequestMapping( "/register" )
 public class register{
 
@@ -11,10 +13,20 @@ public class register{
     @GetMapping( "/addUser" )
     public Result addUser () {
         try {
-
+            System.out.println( "收到" );
             return new Result( "注册成功" , true   ) ;
         } catch ( Exception e ) {
             return new Result( "注册失败" , false ) ;
+        }
+    }
+
+    //  发送验证码
+    @GetMapping( "/sendVerifyCode" )
+    public Result sendVerityCode () {
+        try {
+            return new Result( "注册成功" , true   ) ;
+        } catch ( Exception e ) {
+            return new Result( "发送验证码失败" , false ) ;
         }
     }
 }
